eac7d721b064fb59527de4d7f07f0dbb3e4ed94e
milosz.kucharski <miloszkucharski@gmail.com>
2024-12-24 22:49:41 +0100

    updated run

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ ! -d "venv" ]; then
-    python3 -m venv venv
+    python -m venv venv
 fi
 
-source venv/bin/activate
+source venv/bin/activate;
 
-pip install --no-warn-script-location -r requirements.txt
+pip install --no-warn-script-location -r requirements.txt;
 
 python main.py
